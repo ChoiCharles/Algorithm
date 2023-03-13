92a61c30b5add261ff5b1ebbe2cbd7adf10b961a
ChoiCharles <ckstjr3592@naver.com>
2023-03-13 23:15:53 +0900

    230313

---
+++ baek_2750.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = []
+for i in range(n):
+    a = int(input())
+    lst.append(a)
+for i in range(n-1):
+    for j in range(n-1-i):
+        if lst[j] > lst[j+1]:
+            lst[j], lst[j+1] = lst[j+1], lst[j]
+for i in lst:
+    print(i)
